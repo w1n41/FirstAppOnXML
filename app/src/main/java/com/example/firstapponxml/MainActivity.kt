2d77194354f0b1af0d776d7d606f768159f33a74
w1n41 <dima200.dkv@gmail.com>
2024-12-12 23:37:17 +0500

    Initial commit

---
+++ app/src/main/java/com/example/firstapponxml/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.firstapponxml
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.root_layout)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun clearForm(view: View){
+        findViewById<EditText>(R.id.et_user_name).text.clear()
+        findViewById<EditText>(R.id.et_user_mail).text.clear()
+        findViewById<EditText>(R.id.et_user_message).text.clear()
+    }
+}
